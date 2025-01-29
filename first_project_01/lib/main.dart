bfaf83e8bcc942caa98780d67ef88b17f414b63a
Nathnael Zelalem <nathnaelzelalem@gmail.com>
2025-01-29 22:22:52 +0300

    Add Custom widget

--- first_project_01/lib/main.dart
+++ first_project_01/lib/main.dart
@@ -2,23 +2,27 @@ import 'package:flutter/material.dart';
 
 void main() {
   runApp(MaterialApp(
-    home: Scaffold(
-      body: Container(
-        decoration: BoxDecoration(
-          gradient: LinearGradient(colors: [
-            const Color.fromARGB(255, 24, 2, 63),
-            const Color.fromARGB(255, 67, 2, 87)
-          ], begin: Alignment.topLeft, end: Alignment.bottomRight),
-        ),
-        child: Center(
-          child: Text(
-            "Hello World",
-            style: TextStyle(
-                color: const Color.fromARGB(255, 216, 166, 17), fontSize: 28),
-          ),
-        ),
-      ),
-    ),
+    home: Scaffold(body: GradientContainer()),
   ));
 }
-/// lecture 014
+
+class GradientContainer extends StatelessWidget {
+  @override
+  Widget build(context) {
+    return Container(
+      decoration: BoxDecoration(
+        gradient: LinearGradient(colors: [
+          const Color.fromARGB(255, 24, 2, 63),
+          const Color.fromARGB(255, 67, 2, 87)
+        ], begin: Alignment.topLeft, end: Alignment.bottomRight),
+      ),
+      child: Center(
+        child: Text(
+          "Hello World",
+          style: TextStyle(
+              color: const Color.fromARGB(255, 216, 166, 17), fontSize: 28),
+        ),
+      ),
+    );
+  }
+}
