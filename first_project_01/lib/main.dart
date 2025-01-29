8223e589ad7cb9efacf176fb09e97a8427c7b544
Nathnael Zelalem <nathnaelzelalem@gmail.com>
2025-01-29 21:07:39 +0300

    add color wedjet

--- first_project_01/lib/main.dart
+++ first_project_01/lib/main.dart
@@ -3,9 +3,11 @@ import 'package:flutter/material.dart';
 void main() {
   runApp(MaterialApp(
     home: Scaffold(
+      backgroundColor: Colors.deepPurple,
       body: Center(
         child: Text("Hello World"),
       ),
     ),
   ));
 }
+/// lecture 014
