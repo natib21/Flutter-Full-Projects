cd90b3259477258a1c967246e9d0cee9757b7a82
Nathnael Zelalem <nathnaelzelalem@gmail.com>
2025-01-29 21:52:39 +0300

    Styling Text

--- first_project_01/lib/main.dart
+++ first_project_01/lib/main.dart
@@ -5,15 +5,17 @@ void main() {
     home: Scaffold(
       body: Container(
         decoration: BoxDecoration(
-          gradient: LinearGradient(
-            colors: [
-              const Color.fromARGB(255, 24, 2, 63),
-              const Color.fromARGB(255, 34, 2, 87)
-            ],
-          ),
+          gradient: LinearGradient(colors: [
+            const Color.fromARGB(255, 24, 2, 63),
+            const Color.fromARGB(255, 67, 2, 87)
+          ], begin: Alignment.topLeft, end: Alignment.bottomRight),
         ),
         child: Center(
-          child: Text("Hello World"),
+          child: Text(
+            "Hello World",
+            style: TextStyle(
+                color: const Color.fromARGB(255, 216, 166, 17), fontSize: 28),
+          ),
         ),
       ),
     ),
