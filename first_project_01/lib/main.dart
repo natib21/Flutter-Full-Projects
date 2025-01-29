9ee78a5efaa5f198e3baccf9a61dc7572731ca18
Nathnael Zelalem <nathnaelzelalem@gmail.com>
2025-01-29 21:28:56 +0300

    add gradient with container

--- first_project_01/lib/main.dart
+++ first_project_01/lib/main.dart
@@ -3,9 +3,18 @@ import 'package:flutter/material.dart';
 void main() {
   runApp(MaterialApp(
     home: Scaffold(
-      backgroundColor: Colors.deepPurple,
-      body: Center(
-        child: Text("Hello World"),
+      body: Container(
+        decoration: BoxDecoration(
+          gradient: LinearGradient(
+            colors: [
+              const Color.fromARGB(255, 24, 2, 63),
+              const Color.fromARGB(255, 34, 2, 87)
+            ],
+          ),
+        ),
+        child: Center(
+          child: Text("Hello World"),
+        ),
       ),
     ),
   ));
