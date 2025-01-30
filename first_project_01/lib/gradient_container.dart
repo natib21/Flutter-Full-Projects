d8a2adf1770d3fd626f6dcca2e09e71d375c9bdf
Nathnael Zelalem <nathnaelzelalem@gmail.com>
2025-01-30 16:23:24 +0300

    add image to mobile

--- first_project_01/lib/gradient_container.dart
+++ first_project_01/lib/gradient_container.dart
@@ -1,21 +1,25 @@
 import 'package:flutter/material.dart';
-import 'package:_01_first_project/styled_text.dart';
 
 var startAlignment = Alignment.topLeft;
 var endAlignment = Alignment.bottomRight;
 
 class GradientContainer extends StatelessWidget {
-  const GradientContainer(this.color, {super.key});
+  const GradientContainer(this.color1, this.color2, {super.key});
+  const GradientContainer.purple({super.key})
+      : color1 = Colors.deepPurple,
+        color2 = Colors.indigo;
 
-  final List<Color> color;
+  final Color color1;
+  final Color color2;
   @override
   Widget build(context) {
     return Container(
-      decoration: BoxDecoration(
-        gradient: LinearGradient(
-            colors: color, begin: startAlignment, end: endAlignment),
-      ),
-      child: Center(child: StyledText("Hello World")),
-    );
+        decoration: BoxDecoration(
+          gradient: LinearGradient(
+              colors: [color1, color2],
+              begin: startAlignment,
+              end: endAlignment),
+        ),
+        child: Center(child: Image.asset('assets/images/dice-1.png')));
   }
 }
--- first_project_01/lib/main.dart
+++ first_project_01/lib/main.dart
@@ -3,8 +3,10 @@ import 'package:_01_first_project/gradient_container.dart';
 
 void main() {
   runApp(MaterialApp(
-    home: Scaffold(
-        body: GradientContainer(
-            [Color.fromARGB(255, 24, 2, 63), Color.fromARGB(255, 67, 2, 87)])),
-  ));
+      home: Scaffold(
+    body: GradientContainer.purple(),
+  )));
 }
+
+
+// lecture 032  
